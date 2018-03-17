3b17c9b84b2d7dac23b23c3471aca1f36390f996
sultana khatoon <khatoonsultana0@gmail.com>
2018-03-17 19:50:10 +0530

    RxJs

---
+++ lesson.js
@@ -0,0 +1,15 @@
+﻿const observalable = Rx.observalable.create(observer => {
+    observer.next('Hello')
+    observer.next( 'world' )
+})
+
+observalable.subscribe(val => print(val))
+
+
+
+
+function print(val) {
+    let e1 = document.createElement('p')
+    e1.innerText = val
+    document.body.appendChild(e1)
+}
